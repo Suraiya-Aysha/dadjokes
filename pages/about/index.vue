13c59a1b9ea4b9b1d84a2f498cab322d619f33ca
suraiya-aysha <sm.asa15nov@gmail.com>
2020-08-30 03:02:44 +0600

    Dadjoks project done

---
+++ pages/about/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>About page is here</h1>
+  <!-- this is for test purpose. -->
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About The App",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corney dad jokes"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/contact/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Main Contact Here</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
